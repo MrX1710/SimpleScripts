9d8d9afbe6eea062a087d1b2756fb775e4c102d8
MrX1710 <167465538+MrX1710@users.noreply.github.com>
2024-05-22 09:42:29 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -19,7 +19,7 @@ to install the package, and also if you have nala
 already because it's the default package that will
 be used for the installation, if not then the script will
 use apt as the default package, feel free to modify
-the script if you want. LONG LIFE FOR OPEN SOURCE CODE!!
+the script if you want.
 "
 }
 
