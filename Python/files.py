46350410bde3e640ee4f1b9f959c57b4a6d17375
MrX1710 <167465538+MrX1710@users.noreply.github.com>
2025-08-09 01:45:37 +0200

    .

---
+++ Python/files.py
@@ -0,0 +1,23 @@
+import re
+from sys import exit
+try:
+    print("Hello user")
+    FILENAME = input("Name your new file's name: ")
+    FILE_TYPE = input("Enter the file type: ")
+
+    while not re.match(r'^\.[\w]+$', FILE_TYPE):
+        FILE_TYPE = input("Enter the file type: ")
+    else:
+        FILENAME+= FILE_TYPE
+
+    try:
+        with open( FILENAME , 'x') as f:
+            TEXT = input("Enter the text you want to insert into the file > ")
+            if TEXT.strip() == "":
+                exit()
+            else:
+                f.write(TEXT + "\n")
+    except FileExistsError as FEE:
+        print(FEE)
+except KeyboardInterrupt as KI:
+    print(KI)
